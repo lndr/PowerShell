4f82b2859726d248547683c05f22a11a6c03f83f
Leander Claes <claes@emt.uni-paderborn.de>
2023-01-09 11:18:13 +0100

    Use different hostname method
    This one actually returns the full name, not only the first 15 characters, and is already in lower case.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ Set-PSReadlineKeyHandler -Key "DownArrow" -Function HistorySearchForward
 
 function Prompt{
     $user = $env:USERNAME
-    $hostname = $env:COMPUTERNAME.ToLower()
+    $hostname = hostname
     $directory = Get-Location
     Write-Host("$user@$hostname ") -NoNewline -ForegroundColor "Green" 
     Write-Host("$directory") -ForegroundColor "Blue" 
