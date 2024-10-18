01fd0af38fa4ca9660a2c3c1e7ad8137c608f7b6
Leander Claes <claes@emt.uni-paderborn.de>
2024-10-18 11:33:12 +0200

    Add host name autocomplete
    Thanks: https://gist.github.com/backerman/2c91d31d7a805460f93fe10bdfa0ffb0

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,38 @@
+# Host name autocompletion
+using namespace System.Management.Automation
+Register-ArgumentCompleter -CommandName ssh,scp,sftp -Native -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    $knownHosts = Get-Content ${Env:HOMEPATH}\.ssh\known_hosts `
+    | ForEach-Object { ([string]$_).Split(' ')[0] } `
+    | ForEach-Object { $_.Split(',') } `
+    | Sort-Object -Unique
+    # For now just assume it's a hostname.
+    $textToComplete = $wordToComplete
+    $generateCompletionText = {
+        param($x)
+        $x
+    }
+    if ($wordToComplete -match "^(?<user>[-\w/\\]+)@(?<host>[-.\w]+)$") {
+        $textToComplete = $Matches["host"]
+        $generateCompletionText = {
+            param($hostname)
+            $Matches["user"] + "@" + $hostname
+        }
+    }
+    $knownHosts `
+    | Where-Object { $_ -like "${textToComplete}*" } `
+    | ForEach-Object { [CompletionResult]::new((&$generateCompletionText($_)), $_, [CompletionResultType]::ParameterValue, $_) }
+}
+
+# Extension modules
 Import-Module posh-git
 
+# History searching using arrow keys
 Set-PSReadlineKeyHandler -Key "Tab" -Function MenuComplete
 Set-PSReadlineKeyHandler -Key "UpArrow" -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key "DownArrow" -Function HistorySearchForward
 
+# Prompt configuration
 function Prompt{
     $user = $env:USERNAME
     $hostname = hostname
@@ -13,4 +42,5 @@ function Prompt{
     ">: "
 }
 
+# Clear shell
 cls
